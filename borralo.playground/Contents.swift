301ba5b75ec6d986a2d9f73d44a334ce11f9a6d1
sheyla fuentes <sgf763@mail.missouri.edu>
2018-03-22 09:36:19 -0500

    build functionality

---
+++ borralo.playground/Contents.swift
@@ -0,0 +1,6 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+class RestaurantsViewController  superclass called UIViewController.  RestaurantsViewController also adopts the protocols UITableViewDataSource and UITableViewDelegate.  Which of the following is the correct way to declare the RestaurantsViewController class?
